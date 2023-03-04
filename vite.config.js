4f5fc9d14d40586cc0e60625f74387b1077499df
Sh3lby007 <zhengshaobin@ymail.com>
2023-03-04 18:32:43 +0800

    Convert it to PWA and update npm packages.

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,35 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      }
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+// https://vitejs.dev/config/#conditional-config
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      vue(),
+
+      VitePWA({
+        injectRegister: "auto",
+        registerType: "autoUpdate",
+        devOptions: {
+          enabled: true,
+        },
+        manifest: {
+          name: "SunnySideUp",
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    // When deploying to Github pages, the base URL will be your repo's name,
+    // Thus the production base URL must be changed here for it to work when deployed.
+    // Ref: https://docs.github.com/en/pages/getting-started-with-github-pages/creating-a-github-pages-site#next-steps
+    base: mode === "github-pages" ? "/SunnySideUp/" : "/",
+  };
+});
